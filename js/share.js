080e509b542d00751e273a908985be69d8233a4e
eunsuneun <eunsunbsn@gmail.com>
2022-01-08 15:27:33 +0900

    Edit css-back button & js-줄바꿈

--- js/share.js
+++ js/share.js
@@ -2,7 +2,7 @@ const url = 'https://twelve-animal-type.netlify.app';
 function setShare(){
   const resultAlt = document.querySelector('.result-img').firstElementChild.alt;
   const shareTit = infoList[resultAlt].name;
-  const shareDesc = '십이간지로 알아보는 나의 연애유형! 나의 연애유형은 어떤 타입?';
+  const shareDesc = '십이간지로 알아보는 나의 연애유형!\n나의 연애유형은 어떤 타입?';
   const shareImg = url + '/img/image-' + resultAlt + '.png';
   const shareURL = url + '/pages/' + resultAlt + '.html';
 
